7e452e123fded52fbdbe3660bd010ea893db850e
Arzoo Parwez <arzoobsc@gmail.com>
2019-07-16 22:10:13 +0530

    Operators

---
+++ src/forloop_1.kt
@@ -0,0 +1,8 @@
+fun main(){
+    val names = arrayOf("jim","john","jenny","jamie")
+
+    for (index in names.indices){
+        println("Index: $index Elements: ${names[index]}")
+    }
+
+}
---
+++ src/operators.kt
@@ -0,0 +1,11 @@
+fun main(){
+    println("2 > 1 is ${2 > 1}")
+    println("2 < 1 is ${2 < 1}")
+    println("1 >= 1 is ${1 >= 1}")
+
+    println("1 == 1 is ${1 == 1}")
+    println("1 == 2 is ${1 == 2}")
+    println("1 != 2 is ${1 != 2}")
+//    Logical NOT Operator
+    println("!(2 > 1) is ${!(2 > 1)}")
+}
