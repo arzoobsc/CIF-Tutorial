1dc5d3e032483ff2ec65d45f9510284e1952ada0
Arzoo Parwez <arzoobsc@gmail.com>
2019-07-16 22:53:53 +0530

    Operators

---
+++ src/HelloWorld.kt
@@ -0,0 +1,14 @@
+fun main(){
+    println("Hello! World")
+//    print Hello! World by calling a function
+    Outer()
+}
+
+fun Outer(){
+    print("By Outer Function : Hello! World")
+}
+/*
+*   Output =>
+* Hello! World
+By Outer Function : Hello! World
+* */
