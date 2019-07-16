9e33338d5d91d5046126cec2fd6c685325ecf5d2
Arzoo Parwez <arzoobsc@gmail.com>
2019-07-16 22:32:29 +0530

    Operators

--- src/operators.kt
+++ src/operators.kt
@@ -8,4 +8,23 @@ fun main(){
     println("1 != 2 is ${1 != 2}")
 //    Logical NOT Operator
     println("!(2 > 1) is ${!(2 > 1)}")
+//    Logical AND Operator
+    println("true && false = ${true && false}")
+    /*
+    * true && true = true
+    * true && false = false
+    * false && true = false
+    * false && false = false
+    * */
+//    Logical OR Operator
+    println("true || false = ${true || false}")
+    /*
+    * true || true = true
+    * true || false = true
+    * false || true = true
+    * false || false = false
+    * */
+    val condition = (2 > 1) || (3 > 2) && (2 > 3)
+    println(condition)
+
 }
